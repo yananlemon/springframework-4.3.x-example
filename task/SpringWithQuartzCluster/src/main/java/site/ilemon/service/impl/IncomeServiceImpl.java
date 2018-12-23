c62cc06252052bf141f4f5c5e3f9810d23d7e22c
yanan <yananzx@yeah.net>
2018-12-23 19:45:29 +0800

    quartz

--- task/SpringWithQuartzCluster/src/main/java/site/ilemon/service/impl/IncomeServiceImpl.java
+++ task/SpringWithQuartzCluster/src/main/java/site/ilemon/service/impl/IncomeServiceImpl.java
@@ -1,5 +1,9 @@
 package site.ilemon.service.impl;
 
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+import org.quartz.DisallowConcurrentExecution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,20 +11,31 @@ import site.ilemon.dao.IncomeDao;
 import site.ilemon.service.IIncomeService;
 
 @Service
-public class IncomeServiceImpl implements IIncomeService{
+@DisallowConcurrentExecution
+public class IncomeServiceImpl implements IIncomeService ,Serializable{
 
-	@Autowired
-	private IncomeDao dao;
+	private static Logger logger = Logger.getLogger(IncomeServiceImpl.class);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4768106617997270899L;
+	//@Autowired
+	//private IncomeDao dao;
 	
 	/**
 	 * 这里只是为了展示功能，故直接更新所有用户的金额
 	 */
 	@Override
 	public int updateTotalAmt() {
-		System.out.println("开始更新收益");
-		int rs = dao.updateTotalAmt();
-		System.out.println("更新收益结束");
-		return rs;
+		logger.info("开始执行定时任务");
+		//int rs = dao.updateTotalAmt();
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		logger.info("定时任务执行完毕");
+		return 0;
 	}
 
 }
--- task/SpringWithQuartzCluster/src/main/java/site/ilemon/test/Service.java
+++ task/SpringWithQuartzCluster/src/main/java/site/ilemon/test/Service.java
@@ -1,13 +1,19 @@
 package site.ilemon.test;
 
+import java.io.Serializable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import site.ilemon.service.IIncomeService;
 
 @Component("service")
-public class Service {
+public class Service implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1528716619256407967L;
 	@Autowired
 	private IIncomeService service;
 	
